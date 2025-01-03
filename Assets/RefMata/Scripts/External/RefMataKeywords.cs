2dbdf053fcdcd68d77d3f550a207491698132bc3
riru-ruribe <riru.ruribe@gmail.com>
2025-01-03 14:45:12 +0900

    移動

--- Assets/RefMata/Scripts/External/RefMataKeywords.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RefMata
-{
-    public static partial class RefMataKeywords
-    {
-        public const string Name = "RefMata";
-    }
-}
---
+++ Assets/RefMata/Scripts/Runtime/RefMataKeywords.cs
@@ -0,0 +1,8 @@
+﻿namespace RefMata
+{
+    public static class RefMataKeywords
+    {
+        public const string Name = "RefMata";
+        public const string Compile = nameof(Compile);
+    }
+}
